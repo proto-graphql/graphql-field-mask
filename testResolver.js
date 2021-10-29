dc3b15e8d99074979a1f48b56e0d5e1c3685aa47
izumin5210 <m@izum.in>
2021-10-30 00:39:41 +0900

    Test with multi-version graphql-js

---
+++ testResolver.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = (request, options) => {
+  const modulesDir = path.join(__dirname, "node_modules");
+  return options.defaultResolver(request, {
+    ...options,
+    pathFilter: (pkg, abspath, relativePath) => {
+      if (pkg.name === "graphql" && process.env.GRAPHQLJS_VERSION) {
+        return path.join(modulesDir, `graphql-${process.env.GRAPHQLJS_VERSION}`, relativePath);
+      }
+      return abspath;
+    },
+  });
+};
